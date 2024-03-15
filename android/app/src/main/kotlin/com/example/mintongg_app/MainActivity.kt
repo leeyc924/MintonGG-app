e82493efc223cf07852f6678fbb2dee932c723d3
leeyc924 <leeyc924@naver.com>
2024-03-16 08:54:44 +0900

    Init flutter project

---
+++ android/app/src/main/kotlin/com/example/mintongg_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mintongg_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'controllers/webview_controller.dart';
+
+void main() {
+  Get.put(WebviewMainController());
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(Uri.parse('https://cholol.tistory.com/595'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
